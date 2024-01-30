20941bc05507c6c1c2addb1f06975054367c9272
Poem Maharjan <109945333+pop-o@users.noreply.github.com>
2024-01-30 13:03:17 +0545

    Update ColorGen.js

--- RandomColorGenerator/ColorGen.js
+++ RandomColorGenerator/ColorGen.js
@@ -4,10 +4,10 @@ function generate(){
     const red=(Math.floor(Math.random()*1000))%255;
     const green=(Math.floor(Math.random()*1000))%255;
     const blue=(Math.floor(Math.random()*1000))%255;
-    box[0].style.backgroundColor=`rgb(${red},${green},${blue})`;
-    box[1].style.backgroundColor=`rgb(${green},${blue},${red})`;
-    box[2].style.backgroundColor=`rgb(${blue},${red},${green})`;
-    box[3].style.backgroundColor=`rgb(${blue},${green},${red})`;
-    box[4].style.backgroundColor=`rgb(${red},${blue},${red})`;
-    box[5].style.backgroundColor=`rgb(${blue},${green},${blue})`;
+    box[0].style.backgroundColor=`rgb(${red-green},${green},${blue})`;
+    box[1].style.backgroundColor=`rgb(${green-red},${blue+red},${red*2})`;
+    box[2].style.backgroundColor=`rgb(${blue+green},${red},${green+red-blue})`;
+    box[3].style.backgroundColor=`rgb(${blue+red},${green},${red})`;
+    box[4].style.backgroundColor=`rgb(${red/2},${blue+red},${red})`;
+    box[5].style.backgroundColor=`rgb(${blue+blue},${green+green},${blue*red})`;
 }
