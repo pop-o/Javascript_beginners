a9ab2d17069fa706bbac470209f2bfa571c94c6c
Poem Maharjan <109945333+pop-o@users.noreply.github.com>
2024-01-30 14:07:50 +0545

    Update RCG.js

--- RandomColorCodeGenerator/RCG.js
+++ RandomColorCodeGenerator/RCG.js
@@ -7,19 +7,45 @@ reset_btn.addEventListener(
     "click",reset
 )
 const box=document.getElementById("box");
+var x=0;
 function generate(){
+    if(x==0){
     var ranNum=Math.floor(Math.random()*16777215);
-    var ranCode="#"+ranNum.toString(16);
-    console.log(ranCode);
-    
+    var ranCode="#"+ranNum.toString(16);  
     box.innerText=ranCode;
     box.style.backgroundColor=ranCode;
+    box.style.fontSize="40px";
     navigator.clipboard.writeText(ranCode);
+}else {
+    alert("reset first");
+}
 }
 generate();
 
 function reset(){
     box.style.backgroundColor="#ffffff";
     box.innerText="#ffffff";
+    if(x==1){
+    box.style.filter='none';
+    }
     navigator.clipboard.writeText("#ffffff");
+    x=0;
+
 }
+
+function invert(){
+    code=document.getElementById("box").textContent;
+    if(x==0){
+   box.style.filter='invert(100%)';
+   
+   box.innerText=code+"\n inverted";
+   x=1;
+}else if(x==1){
+    box.style.filter='none';
+    box.innerText=code.replace(" inverted","");
+    x=0;
+}else return;
+}
+
+var invert_btn=document.getElementById("invert-btn");
+invert_btn.addEventListener("mousedown",invert);
